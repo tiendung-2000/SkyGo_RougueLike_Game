fb31bb00fc0ff82879ad9fbbeb7876c1d0ab5e2a
tiendung-2000 <tiendung2000tn@gmail.com>
2023-04-11 00:35:22 +0700

    Map and Boss

--- Assets/_Roguelike_Game/Scripts/CharacterTracker.cs
+++ Assets/_Roguelike_Game/Scripts/CharacterTracker.cs
@@ -2,12 +2,5 @@ using UnityEngine;
 
 public class CharacterTracker : Singleton<CharacterTracker>
 {
-    //public static CharacterTracker Ins;
-
     public int currentHealth, maxHealth, currentCoins;
-
-    //private void Awake()
-    //{
-    //    Ins = this;
-    //}
 }
